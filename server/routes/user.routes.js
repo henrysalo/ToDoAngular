5297bd8106fb20f6132a6d08c0530b55e9f08406
Henry Salomón <64495992+henrysalo@users.noreply.github.com>
2021-08-22 18:26:32 -0500

    eliminación componentes innecesarios en servidor

--- server/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerUser = express.Router();
-
-const userCtrl = require('../controllers/user.controller')
-
-routerUser.post('/', userCtrl.getUser);
-routerUser.post('/', userCtrl.createEmployee);
-
-module.exports = routerUser;
